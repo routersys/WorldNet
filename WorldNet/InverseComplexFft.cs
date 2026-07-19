dd40aa9450ae1c3190ab528a2c12670709c5cde8
routersys <github.routersys@gmail.com>
2026-07-19 21:23:20 +0900

    逆向き複素FFT構造を移植

---
+++ WorldNet/InverseComplexFft.cs
@@ -0,0 +1,37 @@
+namespace WorldNet;
+
+internal unsafe struct InverseComplexFft
+{
+    public int FftSize;
+    public FftComplex* Input;
+    public FftComplex* Output;
+    public FftPlan InverseFft;
+
+    public static void Layout<TAllocator>(ref TAllocator allocator, int fftSize,
+        ref InverseComplexFft fft)
+        where TAllocator : struct, IScratchAllocator
+    {
+        fft.Input = (FftComplex*)allocator.Allocate(fftSize, (nuint)sizeof(FftComplex));
+        fft.Output = (FftComplex*)allocator.Allocate(fftSize, (nuint)sizeof(FftComplex));
+        FftPlan.Layout(ref allocator, fftSize, FftPlanKind.ComplexToComplex, ref fft.InverseFft);
+    }
+
+    public static nuint GetRequiredArenaBytes(int fftSize)
+    {
+        MeasuringAllocator allocator = default;
+        InverseComplexFft fft = default;
+        Layout(ref allocator, fftSize, ref fft);
+        return allocator.Total;
+    }
+
+    public static InverseComplexFft Bind(WorldArena arena, int fftSize)
+    {
+        ArenaAllocator allocator = new(arena);
+        InverseComplexFft fft = default;
+        Layout(ref allocator, fftSize, ref fft);
+        fft.FftSize = fftSize;
+        fft.InverseFft.InitializeComplexToComplex(
+            fftSize, fft.Input, fft.Output, FftDirection.Backward);
+        return fft;
+    }
+}
