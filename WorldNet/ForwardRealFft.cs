5b63fcc9fd8edc929edd797a414fa76114e5eb6b
routersys <github.routersys@gmail.com>
2026-07-19 21:23:19 +0900

    前向き実FFT構造を移植

---
+++ WorldNet/ForwardRealFft.cs
@@ -0,0 +1,36 @@
+namespace WorldNet;
+
+internal unsafe struct ForwardRealFft
+{
+    public int FftSize;
+    public double* Waveform;
+    public FftComplex* Spectrum;
+    public FftPlan ForwardFft;
+
+    public static void Layout<TAllocator>(ref TAllocator allocator, int fftSize,
+        ref ForwardRealFft fft)
+        where TAllocator : struct, IScratchAllocator
+    {
+        fft.Waveform = (double*)allocator.Allocate(fftSize, sizeof(double));
+        fft.Spectrum = (FftComplex*)allocator.Allocate(fftSize, (nuint)sizeof(FftComplex));
+        FftPlan.Layout(ref allocator, fftSize, FftPlanKind.RealToComplex, ref fft.ForwardFft);
+    }
+
+    public static nuint GetRequiredArenaBytes(int fftSize)
+    {
+        MeasuringAllocator allocator = default;
+        ForwardRealFft fft = default;
+        Layout(ref allocator, fftSize, ref fft);
+        return allocator.Total;
+    }
+
+    public static ForwardRealFft Bind(WorldArena arena, int fftSize)
+    {
+        ArenaAllocator allocator = new(arena);
+        ForwardRealFft fft = default;
+        Layout(ref allocator, fftSize, ref fft);
+        fft.FftSize = fftSize;
+        fft.ForwardFft.InitializeRealToComplex(fftSize, fft.Waveform, fft.Spectrum);
+        return fft;
+    }
+}
