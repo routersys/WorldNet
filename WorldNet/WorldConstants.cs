131ce4386db60e1965794792d95c5ec487ee0a20
routersys <github.routersys@gmail.com>
2026-07-19 20:44:24 +0900

    原典の定数定義を移植

---
+++ WorldNet/WorldConstants.cs
@@ -0,0 +1,30 @@
+namespace WorldNet;
+
+internal static class WorldConstants
+{
+    public const double CutOff = 50.0;
+
+    public const double FloorF0StoneMask = 40.0;
+
+    public const double Pi = 3.1415926535897932384;
+    public const double MySafeGuardMinimum = 0.000000000001;
+    public const double Eps = 0.00000000000000022204460492503131;
+    public const double FloorF0 = 71.0;
+    public const double CeilF0 = 800.0;
+    public const double DefaultF0 = 500.0;
+    public const double Log2 = 0.69314718055994529;
+    public const double MaximumValue = 100000.0;
+
+    public const int Hanning = 1;
+    public const int Blackman = 2;
+    public const double FrequencyInterval = 3000.0;
+    public const double UpperLimit = 15000.0;
+    public const double Threshold = 0.85;
+    public const double FloorF0D4C = 47.0;
+    public const double SafeGuardD4C = 0.000001;
+
+    public const double MelM0 = 1127.01048;
+    public const double MelF0 = 700.0;
+    public const double FloorFrequency = 40.0;
+    public const double CeilFrequency = 20000.0;
+}
