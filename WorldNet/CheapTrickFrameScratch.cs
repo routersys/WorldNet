012913474e2823699a93a55c69765198ca5e3fe8
routersys <github.routersys@gmail.com>
2026-07-19 22:32:18 +0900

    CheapTrickの枠単位作業領域を型として定義

---
+++ WorldNet/CheapTrickFrameScratch.cs
@@ -0,0 +1,31 @@
+namespace WorldNet;
+
+internal unsafe struct CheapTrickFrameScratch
+{
+    public int* BaseIndex;
+    public int* SafeIndex;
+    public double* Window;
+    public DcCorrectionScratch DcCorrection;
+    public LinearSmoothingScratch LinearSmoothing;
+
+    public static void Layout<TAllocator>(ref TAllocator allocator, int halfWindowLength,
+        int upperLimit, int fftSize, int boundary, ref CheapTrickFrameScratch scratch)
+        where TAllocator : struct, IScratchAllocator
+    {
+        int windowLength = (halfWindowLength * 2) + 1;
+        scratch.BaseIndex = (int*)allocator.Allocate(windowLength, sizeof(int));
+        scratch.SafeIndex = (int*)allocator.Allocate(windowLength, sizeof(int));
+        scratch.Window = (double*)allocator.Allocate(windowLength, sizeof(double));
+        DcCorrectionScratch.Layout(ref allocator, upperLimit, ref scratch.DcCorrection);
+        LinearSmoothingScratch.Layout(ref allocator, fftSize, boundary, ref scratch.LinearSmoothing);
+    }
+
+    public static CheapTrickFrameScratch Bind(WorldArena arena, int halfWindowLength,
+        int upperLimit, int fftSize, int boundary)
+    {
+        ArenaAllocator allocator = new(arena);
+        CheapTrickFrameScratch scratch = default;
+        Layout(ref allocator, halfWindowLength, upperLimit, fftSize, boundary, ref scratch);
+        return scratch;
+    }
+}
