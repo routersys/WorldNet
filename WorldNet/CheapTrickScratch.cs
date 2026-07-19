ddce15c8e263021c9e155ea905e611b7f6bfb05a
routersys <github.routersys@gmail.com>
2026-07-19 22:32:18 +0900

    CheapTrickの作業領域を型として定義

---
+++ WorldNet/CheapTrickScratch.cs
@@ -0,0 +1,31 @@
+namespace WorldNet;
+
+internal unsafe struct CheapTrickScratch
+{
+    public ForwardRealFft ForwardRealFft;
+    public InverseRealFft InverseRealFft;
+    public double* SpectralEnvelope;
+    public double* SmoothingLifter;
+    public double* CompensationLifter;
+
+    public static void Layout<TAllocator>(ref TAllocator allocator, int fftSize,
+        ref CheapTrickScratch scratch)
+        where TAllocator : struct, IScratchAllocator
+    {
+        ForwardRealFft.Layout(ref allocator, fftSize, ref scratch.ForwardRealFft);
+        InverseRealFft.Layout(ref allocator, fftSize, ref scratch.InverseRealFft);
+        scratch.SpectralEnvelope = (double*)allocator.Allocate(fftSize, sizeof(double));
+        scratch.SmoothingLifter = (double*)allocator.Allocate(fftSize, sizeof(double));
+        scratch.CompensationLifter = (double*)allocator.Allocate(fftSize, sizeof(double));
+    }
+
+    public static CheapTrickScratch Bind(WorldArena arena, int fftSize)
+    {
+        ArenaAllocator allocator = new(arena);
+        CheapTrickScratch scratch = default;
+        Layout(ref allocator, fftSize, ref scratch);
+        scratch.ForwardRealFft.Initialize(fftSize);
+        scratch.InverseRealFft.Initialize(fftSize);
+        return scratch;
+    }
+}
